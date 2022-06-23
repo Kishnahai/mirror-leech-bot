f72e48c17b222f44a079bd465ac8b81deb311854
woodcraft5 <sekhfaruk5@gmail.com>
2022-06-23 22:48:29 +0530

    telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -72,7 +72,7 @@ class TelegraphHelper:
                     nxt_page += 1
             self.edit_page(
                 path = path[prev_page],
-                title = 'Mirror-leech-bot Torrent Search',
+                title = 'WOODcraft-bot Torrent Search',
                 content=content
             )
         return
