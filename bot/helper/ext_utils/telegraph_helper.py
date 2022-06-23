f29ee57fba6a839ba97c9a99d25c1d78865ec7ce
woodcraft5 <sekhfaruk5@gmail.com>
2022-06-23 22:44:35 +0530

    telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -78,4 +78,4 @@ class TelegraphHelper:
         return
 
 
-telegraph=TelegraphHelper('Mirror-Leech-Telegram-Bot', 'https://github.com/anasty17/mirror-leech-telegram-bot')
+telegraph=TelegraphHelper('WOODcraft-Telegram-Repo', 'https://github.com/anasty17/mirror-leech-telegram-bot')
