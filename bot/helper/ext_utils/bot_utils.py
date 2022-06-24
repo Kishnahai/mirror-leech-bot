8926644497a4b8ee71ebc50e4ab3424b8511dcc7
woodcraft5 <sekhfaruk5@gmail.com>
2022-06-24 21:48:08 +0530

    bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -140,8 +140,9 @@ def get_readable_message():
                 globals()['COUNT'] -= STATUS_LIMIT
                 globals()['PAGE_NO'] -= 1
         for index, download in enumerate(list(download_dict.values())[COUNT:], start=1):
-            msg += f"<b>Name:</b> <code>{escape(str(download.name()))}</code>"
-            msg += f"\n<b>Status:</b> <i>{download.status()}</i>"
+            
+            msg += f"<b>Mirroring Under Process</b>\n<b>Please Wait...</b>"
+            msg += f"\n\n<b>File Name:</b> <code>{escape(str(download.name()))}</code>"
             if download.status() not in [
                 MirrorStatus.STATUS_ARCHIVING,
                 MirrorStatus.STATUS_EXTRACTING,
