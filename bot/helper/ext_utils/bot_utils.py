3bac3f7b2804897cff918f3564db25b9d0150c77
woodcraft5 <sekhfaruk5@gmail.com>
2022-06-24 22:30:53 +0530

    bot-utlis.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -248,7 +248,10 @@ def get_readable_message():
                     upspeed_bytes += float(spd.split('K')[0]) * 1024
                 elif 'MB/s' in spd:
                     upspeed_bytes += float(spd.split('M')[0]) * 1048576
-        bmsg += f"\n<b>DL:</b> {get_readable_file_size(dlspeed_bytes)}/s | <b>UL:</b> {get_readable_file_size(upspeed_bytes)}/s"
+        
+        bmsg += f"\n\n<b>🎛️<i>Speedometer</i>🎛️</b>\n<b>Download :</b> {get_readable_file_size(dlspeed_bytes)}/s\n<b>Upload      :</b> {get_readable_file_size(upspeed_bytes)}/s"
+
+        # bmsg += f"\n<b>DL:</b> {get_readable_file_size(dlspeed_bytes)}/s | <b>UL:</b> {get_readable_file_size(upspeed_bytes)}/s"
         if STATUS_LIMIT is not None and tasks > STATUS_LIMIT:
             msg += f"<b>Page:</b> {PAGE_NO}/{pages} | <b>Tasks:</b> {tasks}\n"
             buttons = ButtonMaker()
